da59f6a3478c9b4cda7427410cba56255b3afa5f
Alan <alan_pires2@hotmail.com>
2022-01-02 20:44:07 -0300

    depending on number of args it inserts a random number at the end and I dont know why

--- src/parse_data.c
+++ src/parse_data.c
@@ -6,7 +6,7 @@
 /*   By: apires-d <apires-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/30 15:10:29 by apires-d          #+#    #+#             */
-/*   Updated: 2022/01/01 21:05:03 by apires-d         ###   ########.fr       */
+/*   Updated: 2022/01/02 20:38:31 by apires-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 static int	has_repeated(int *args);
 static int	is_valid_int(char **args);
 static int	is_number(char **args);
+static int	check_and_parse(char **str_nums, int **arg_numbers);
 
 int	parse_data(int argc, char *argv[], int **arg_numbers)
 {
@@ -30,21 +31,52 @@ int	parse_data(int argc, char *argv[], int **arg_numbers)
 		if (is_number(str_nums) == FALSE)
 			return (FALSE);
 		while (str_nums[i])
+		{
+			printf("%s\n", str_nums[i]);
 			i++;
-		*arg_numbers = ft_calloc(i, sizeof(int));
+		}
+		*arg_numbers = ft_calloc(i - 1, sizeof(int));
 		if (*arg_numbers)
 		{
-			i = -1;
-			if (is_valid_int(str_nums) ==  FALSE)
+			if (check_and_parse(str_nums, arg_numbers) == FALSE)
 				return (FALSE);
-			while (str_nums[++i])
-				(*arg_numbers)[i] = ft_atoi(str_nums[i]);
-			if (has_repeated(*arg_numbers) == TRUE)
-				return (FALSE);
+			// i = -1;
+			// while ((*arg_numbers)[++i])
+			// {
+			// 	printf("n: %d\n", (*arg_numbers)[i]);
+			// }
 		}
 		else
 			return (FALSE);
 	}
+	// i = -1;
+	// while ((*arg_numbers)[++i])
+	// {
+	// 	printf("%d\n", (*arg_numbers)[i]);
+	// }
+	return (TRUE);
+}
+
+static int	check_and_parse(char **str_nums, int **arg_numbers)
+{
+	int	i;
+
+	if (is_valid_int(str_nums) ==  FALSE)
+		return (FALSE);
+	i = -1;
+	while (str_nums[++i])
+	{
+		(*arg_numbers)[i] = ft_atoi(str_nums[i]);
+	}
+
+	// i = -1;
+	// while ((*arg_numbers)[++i])
+	// {
+	// 	printf("n: %d\n", (*arg_numbers)[i]);
+	// }
+
+	if (has_repeated(*arg_numbers) == TRUE)
+		return (FALSE);
 	return (TRUE);
 }
 
@@ -53,6 +85,14 @@ static int	has_repeated(int *args)
 	int	i;
 	int	j;
 
+	// i = 0;
+	// while (args[i])
+	// {
+	// 	printf("pos: %d\n", i);
+	// 	printf("%d\n", args[i]);
+	// 	i++;
+	// }
+	// printf("\n\n");
 	i = 0;
 	while (args[i])
 	{
@@ -80,6 +120,11 @@ static int	is_valid_int(char **args)
 		if (num < INT_MIN || num > INT_MAX)
 			return (FALSE);
 	}
+	// i = -1;
+	// while (args[++i])
+	// {
+	// 	printf("%s\n", args[i]);
+	// }
 	return (TRUE);
 }
 
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: apires-d <apires-d@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 20:39:15 by apires-d          #+#    #+#             */
-/*   Updated: 2022/01/01 21:02:44 by apires-d         ###   ########.fr       */
+/*   Updated: 2022/01/02 20:39:09 by apires-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,16 @@ int	main(int argc, char *argv[])
 {
 	struct_main	s_main;
 	int			*arg_numbers;
-	int	aux = -1;
+	int	aux = 0;
 
 	if (parse_data(argc, argv, &arg_numbers) == TRUE)
 	{
-		while(arg_numbers[++aux])
+		while(arg_numbers[aux])
+		{
 			printf("%d\n", arg_numbers[aux]);
+			aux++;
+		}
+		printf("OK\n");
 	}
 	else
 		printf("failed\n");
